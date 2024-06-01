830600ae9875f1f3ec2bb2fc84c826af926f4287
Shakira Regalado <136966945+hereskira@users.noreply.github.com>
2024-06-01 14:29:06 +0000

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Shakira",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  email: "shakiraregalado@gmail.com",
+  gitHub: "hereskira",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "shakira-regalado",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
